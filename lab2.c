cd24f5fc92e0e92ad1a1d85fe705a2c1b55e4508
Tina <eakardanova@gmail.com>
2026-03-27 13:53:21 +0300

    add input_file

--- lab2.c
+++ lab2.c
@@ -1,34 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-struct el{
+typedef struct el{
     int data;
-    int key;
-    struct el next;
-    stract el parent;
-}
+    float key;
+    struct el *next;
+    struct el *parent;
+}el;
 
-struct arr{
-    el* head;
-}
 
 void shell_sort(arr* Arr, size){
 
+
+}
+
+void add_element(el* Arr, float key, int data){
+    el* new_el = (el*)malloc(sizeof(el));
+    new_el->next = NULL;
+    new_el->key = key;
+    new_el->data = data;
+    Arr = new_el;
 }
 
 int main(void)
 {
-    arr = {NULL};
+    el arr[15];
     FILE* input_file = fopen("input_file.txt", "r");
     if (input_file == NULL) {
         printf("Error");
         return 0;
     }
 
-    for (int i = 0, 15, i++){ 
-        el* new_el = (el*)malloc(sizeof(el));
-        fscanf(input_file, "%d", &new_el.data);
-        new_el.key = i;
+    for (int i = 0; i < 15; i++){ 
+        int data;
+        float key;
+        fscanf(input_file, "%f %d", &key, &data);
+        add_element(&arr[i], key, data);
     }
+    for (int i = 0; i < 15, i++){
+        printf(arr[i]);
+    }
+    fclose(input_file);
     return 0;
 }
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,1 @@
+delete(9, &test_tree);
